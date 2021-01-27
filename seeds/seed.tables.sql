8b807e154e8b25b87ee66c54500926e236048315
rshuken <ryanshuken@gmail.com>
2021-01-27 00:20:00 -0700

    update language values

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -15,10 +15,10 @@ VALUES
     '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG'
   ),  (
     2,
-    'test',
-    'test',
+    'demotest',
+    'demotest',
     -- password = "pass"
-    '!1Testtest'
+    '1234Qwer!'
   );
 
 INSERT INTO "language" ("id", "name", "user_id", "is_public")
@@ -35,16 +35,16 @@ VALUES
   (6, 1, 'oui', 'yes', 7),
   (7, 1, 'chien', 'dog', 8),
   (8, 1, 'chat', 'cat', null),
-  (9, 2, 'amour', 'love', 10),
-  (10, 2, 'bonjour', 'hello', 11),
-  (11, 2, 'bonheur', 'happiness', 12),
-  (12, 2, 'sourire', 'smile', 13),
-  (13, 2, 'francais', 'french', 14),
-  (14, 2, 'oui', 'yes', 15),
-  (15, 2, 'chien', 'dog', 16),
-  (16, 2, 'chat', 'cat', null);;
+  (9, 2, 'amare', 'love', 10),
+  (10, 2, 'salve', 'hello', 11),
+  (11, 2, 'beatitudinem', 'happiness', 12),
+  (12, 2, 'ridere', 'smile', 13),
+  (13, 2, 'latine', 'latin', 14),
+  (14, 2, 'etiam', 'yes', 15),
+  (15, 2, 'canis', 'dog', 16),
+  (16, 2, 'cattus', 'cat', null);;
 
--- this might be an issue, need to check with Ari - 
+
 UPDATE "language" SET head = 1 WHERE id = 1;
 
 -- because we explicitly set the id fields
@@ -55,4 +55,4 @@ SELECT setval('user_id_seq', (SELECT MAX(id) from "user"));
 
 COMMIT;
 
---  psql -U postgres -d spaced-repetition -f ./seeds/seed.tables.sql
+--  psql -U postgres -d serverName -f ./seeds/seed.tables.sql
